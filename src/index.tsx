5840144b80d0190ddb86f32031c7f0446f0089c4
wilquem <emanuel_wilquem098@hotmail.com>
2022-06-20 23:28:18 -0300

    exclusão miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -8,29 +8,6 @@ createServer({
     transaction: Model,
   },
 
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id:1,
-          title: 'Freelancer de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2022-06-02 09:00:00')
-        },
-        {
-          id:2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2022-06-14 11:00:00')
-        }
-      ]
-    })
-  },
-
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
